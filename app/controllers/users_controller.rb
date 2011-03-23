5e2ab4529ec951b9118893357adc3cb788bec027
Dean Chouinard <deanchouinard@gmail.com>
2011-03-22 23:00:28 -0400

    Added Signup error messages and Signup Success tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      
+      redirect_to @user
     else
       @title = "Sign up"
       render 'new'
