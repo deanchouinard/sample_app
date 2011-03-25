6344a6b221e43413986d68e0076809d32424a1e7
Dean Chouinard <deanchouinard@gmail.com>
2011-03-24 22:45:50 -0400

    Added Flash, Integration tests, and examples

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -50,6 +50,27 @@ describe UsersController do
       get :new
       response.should have_selector("title", :content => "Sign Up")
     end
+    
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end
+
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'][type='password']")
+    end
+
+    it "should have a password_confirmation field" do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+    end
+
   end
   
   describe "POST 'create'" do
@@ -93,6 +114,11 @@ describe UsersController do
       it "should redirect to the user show page" do
         post :create, :user  => @attr
         response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user  => @attr
+        flash[:success].should =~ /welcome to the sample app/i
       end
     end
   end
