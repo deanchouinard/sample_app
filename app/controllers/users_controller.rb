0bc31adddf6f62f5a60791eca8ff15981b10b70d
Dean Chouinard <deanchouinard@gmail.com>
2011-04-06 15:13:00 -0400

    Can delete users (as admins)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
   
   def index
     @users = User.paginate(:page => params[:page])
@@ -45,6 +46,12 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
 
   private
   
@@ -56,4 +63,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
   end
+
+  def admin_user
+    user = User.find(params[:id])
+    redirect_to(root_path) if (!current_user.admin? || current_user?(user))
+  end
+  
 end
