b08a3315e8d628019b257fefd317c8558640df97
Dean Chouinard <deanchouinard@gmail.com>
2011-04-06 12:14:13 -0400

    Working index page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,40 @@ describe UsersController do
 
   render_views
   
+  describe "GET 'index" do
+    describe "for non-signed-in users" do
+      it "should deny access" do
+        get :index
+        response.should redirect_to(signin_path)
+      end      
+    end
+    
+    describe "for signed-in users" do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        Factory(:user, :email =>  "another@example.com")
+        Factory(:user, :email =>  "another@example.net")
+      end
+      
+      it "should be successful" do
+        get :index
+        response.should be_success
+      end
+      
+      it "should have the righ title" do
+        get :index
+        response.should have_selector('title', :content => "All users")
+      end
+      
+      it "should have an element for each user" do
+        get :index
+        User.all.each do |user|
+          response.should have_selector("li", :content => user.name)
+        end
+      end
+    end
+  end
+  
   describe "GET 'show'" do
     before(:each) do
       @user = Factory(:user)
@@ -190,6 +224,15 @@ describe UsersController do
         @user.email.should == @attr[:email]
       end
       
+      it "should change the users attributes V2" do
+        put :update, :id => @user, :user => @attr
+        user = assigns(:user)
+        @user.reload
+        @user.name.should == user.name
+        @user.email.should == user.email
+        @user.encrypted_password.should == user.encrypted_password
+      end
+
       it "should redirect to the user show page" do
         put :update, :id => @user, :user => @attr
         response.should redirect_to(user_path(@user))
